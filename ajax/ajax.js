5e0b7b6c5b9d5b6c71955a351327e4321efe46cf
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2021-12-11 21:26:34 +0800

    hello world,http,variables

--- ajax/ajax.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest()
-xhr.open('GET','https://www.baidu.com',true)
-xhr.onload = function () {
-    console.log(xhr.responseText)
-}
-xhr.send()
---
+++ hello_world/hello_world.js
@@ -0,0 +1,1 @@
+alert("Hello World 外链式")
