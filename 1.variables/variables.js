0dba0e7d2c713a3fbaa623fca94aa225a8a59a10
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2021-12-19 11:15:02 +0800

    2021.12.19

---
+++ 1.variables/variables.js
@@ -0,0 +1,14 @@
+// 声明一个变量，并为其赋值
+var num = 100
+
+// 引用变量
+// 在浏览器弹窗显示 num 变量的值
+alert(num)
+// 在控制台输出 num 变量的值
+console.log(num)
+// 在浏览器页面输出 num 变量的值
+document.write(num + "<br>")
+
+// 变量提升
+document.write(varHoisting)
+var varHoisting = "Variables Hoisting Test"
---
+++ 2.data_type/data_type.js
@@ -0,0 +1,26 @@
+// 数值类型
+var numberType = 2e5
+document.write(numberType)
+document.write("<br>")
+
+// 字符串类型
+var stringType = "string"
+document.write(stringType)
+document.write("<br>")
+
+// 布尔类型
+var booleanType = true
+document.write(booleanType)
+document.write("<br>")
+
+// 空类型
+// null # 有值，但是该值表示为空值
+// undefined # 没有值
+var nullType = null
+var undefinedType
+console.log(nullType)
+console.log(undefinedType)
+
+// 数据类型检测
+var detectDateType = 100
+console.log("检测到的数据类型：", typeof detectDateType)
