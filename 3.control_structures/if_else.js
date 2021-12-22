8d11e4b5c8a025355e9cb0bfd4a543dadc435ce3
DesistDaydream <373406000@qq.com>
2021-12-22 14:02:17 +0800

    控制结构

---
+++ 3.control_structures/if_else.js
@@ -0,0 +1,10 @@
+// if () {...}
+// if () {...} else {...}
+// if () {...} else if () {...}
+// if () {...} else if () {...} else {...}
+var year = 2021
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+    console.log(year + " 是闰年")
+} else {
+    console.log(year + " 不是闰年")
+}
---
+++ 3.control_structures/switch.js
@@ -0,0 +1,26 @@
+// switch (Condition) {
+//     case Value1:
+//         ...
+//     case Value2:
+//         ...
+//     ...
+//     default:
+//         ...
+// }
+
+var foot = 1
+
+switch (foot) {
+    // switch 根据条件判断是否为真，case 中第一个为真的，开始执行其中定义的语句
+    case 37:
+        console.log("37号")
+        // 添加了 break 后，执行完本条件的语句后就会退出 switch
+        break
+    case 40:
+        console.log("40号")
+        // 不添加 break 的话，当 switch 第一次匹配到 case 并执行完成后，不管下一个条件是否匹配，还会继续执行。
+        // 这称为 switch 的穿透效果，从第一个满足条件的 case 开始向下穿透直到遇上退出 switch 的语句
+    default:
+        console.log("默认条件")
+        break
+}
