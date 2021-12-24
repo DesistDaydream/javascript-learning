4a07bc3a0394c1ff0df65c4ad95273717ab3f1ee
DesistDaydream <373406000@qq.com>
2021-12-24 15:10:13 +0800

    2021.12.24

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var objectType = {
+    k1: "1",
+    k2: "2"
+}
+
+var stringType = "Hello_World"
+var stringObjType = new String("Hello_World");
+
+console.log("基本字符串的类型:", typeof stringType)
+console.log("字符串对象的类型:", typeof stringObjType)
+
+
+console.log(stringType)
+console.log(stringObjType)
