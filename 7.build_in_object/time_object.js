bc46857cddf25c19147d5325882282d0222d7655
DesistDaydream <373406000@qq.com>
2021-12-24 17:31:44 +0800

    2021.12.24

---
+++ 7.build_in_object/time_object.js
@@ -0,0 +1,14 @@
+var time = new Date()
+console.log(time)
+
+// 获取时间中的年、月、日、小时、分、秒
+console.log(time.getFullYear())
+console.log(time.getMonth())
+console.log(time.getDate())
+console.log(time.getHours())
+console.log(time.getMinutes())
+console.log(time.getSeconds())
+// 当前是一周中的第几天
+console.log(time.getDay())
+// 获取时间戳
+console.log(time.getTime())
