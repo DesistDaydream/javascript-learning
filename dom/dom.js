08f36878c3e80bd099ee205c2e60f2952c776e52
DesistDaydream <373406000@qq.com>
2021-12-27 17:03:18 +0800

    time 与 dom

---
+++ dom/dom.js
@@ -0,0 +1,10 @@
+// 根据 ID 获取元素
+// 若没有这个 ID，则返回值为 null
+var elementById = document.getElementById("container")
+console.log(elementById)
+
+// 根据类名获取元素
+// 这个方法的参数是一个匹配规则，凡是包含参数中的字符串的类名都会获取到
+// 返回值是一个 HTMLCollection 类型的对象。
+var elementsByClassName = document.getElementsByClassName("box")
+console.log(elementsByClassName)
---
+++ time/time.js
@@ -0,0 +1,23 @@
+// 
+var intervalMs = 1000
+var timeoutMs = 3000
+
+// 间隔定时器
+var intervalTimer = setInterval(function () {
+    console.log("每隔", intervalMs, "毫秒执行一次")
+}, intervalMs);
+
+// 延时定时器
+var timeoutTimer = setTimeout(function () {
+    console.log(timeoutMs, "毫秒后执行")
+}, timeoutMs);
+
+console.log("第一个定时器的返回值:", intervalTimer)
+console.log("第二个定时器的返回值:", timeoutTimer)
+
+closeTimer.onclick = function () {
+    // 通过定时器的返回值，我们可以关闭定时器
+    // clearInterval() 与 clearTimeout() 的效果一样，可以互相使用
+    clearInterval(timeoutTimer)
+    clearTimeout(intervalTimer)
+}
