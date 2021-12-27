6d1b1a1600c3134ca74b4b8d8514d448449061bd
DesistDaydream <373406000@qq.com>
2021-12-27 17:12:21 +0800

    2021.12.27

--- dom/dom.js
+++ dom/dom.js
@@ -5,6 +5,9 @@ console.log(elementById)
 
 // 根据类名获取元素
 // 这个方法的参数是一个匹配规则，凡是包含参数中的字符串的类名都会获取到
-// 返回值是一个 HTMLCollection 类型的对象。
+// 返回值是一个 HTMLCollection 类型的对象
 var elementsByClassName = document.getElementsByClassName("box")
 console.log(elementsByClassName)
+
+// 根据标签名获取元素
+var elementsByTagName = document.getElementsByTagName()
