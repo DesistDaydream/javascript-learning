3915bb74fbb97946eeba8a0262ef6929478bb60d
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-01-12 12:33:48 +0800

    Update event.js

--- dom/event.js
+++ dom/event.js
@@ -1,15 +1,11 @@
 // 事件绑定
 // 事件源.on事件类型=事件处理函数
 var divElement = document.getElementById("event")
-var obj = {}
-var a = new String("string")
-var b = [1, 2, "abc"]
-console.log(typeof divElement)
-console.log(divElement, obj, a)
-console.log(b)
 
-
-// click 点击事件
-divElement.onclick = function () {
+// click 触发一个点击事件
+divElement.onclick = function (prop) {
     console.log(divElement, "元素，被点击了一下")
+    // 事件的属性
+    // 想要获取事件的属性，只需要为事件处理函数添加一个参数即可，这个参数就是事件属性的变量。
+    console.log(prop)
 }
