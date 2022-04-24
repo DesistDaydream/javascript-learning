099d56cb8bea7dc2b7dfd07f680bfef88be522ca
DesistDaydream <373406000@qq.com>
2022-04-24 15:22:03 +0800

    Update constructor.js

--- constructor/constructor.js
+++ constructor/constructor.js
@@ -5,7 +5,13 @@
 //     this.age = age;
 // }
 
-// 使用 ES6 的新语法创建一个对象模板，而不是使用一个意义不够明确的构造函数
+// // Person 的方法，名为 showInfo
+// Person.prototype.showInfo = function () {
+//     return this.name + " is " + this.age + " years old."
+// }
+
+// 使用 ES6 的新语法创建一个对象模板，而不是使用一个意义不够明确的构造函数。
+// 这种写法类似于一个语法糖，让 JS 中面向对象的代码写法与传统面向对象的写法类似
 class Person {
     // 定义一个方法，这个方法会在使用 new() 生成对象时自动调用，类似于 Python 的 class 中的 __init__ 方法
     // 一个 class 中必须包含一个 constructor() 方法，用来定义该 class 的属性。若没有显式定义，则会自动创建一个
@@ -15,9 +21,9 @@ class Person {
         this.age = age;
     }
 
-    // 输出该对象的信息
+    // Person 的方法，名为 showInfo
     showInfo() {
-        console.log(`${this.name} is ${this.age} years old.`);
+        return this.name + " is " + this.age + " years old."
     }
 }
 
