bbf5ae469ee2a31a64776a92288bb3bfdabf8ce5
DesistDaydream <373406000@qq.com>
2022-04-24 15:47:06 +0800

    Update constructor.js

--- constructor/constructor.js
+++ constructor/constructor.js
@@ -13,10 +13,10 @@
 // 使用 ES6 的新语法创建一个对象模板，而不是使用一个意义不够明确的构造函数。
 // 这种写法类似于一个语法糖，让 JS 中面向对象的代码写法与传统面向对象的写法类似
 class Person {
-    // 定义一个方法，这个方法会在使用 new() 生成对象时自动调用，类似于 Python 的 class 中的 __init__ 方法
+    // 定义一个方法，这个方法会在使用 new() 生成对象时自动调用，类似于 Python 中 class 的 __init__ 方法
     // 一个 class 中必须包含一个 constructor() 方法，用来定义该 class 的属性。若没有显式定义，则会自动创建一个
     constructor(name, age) {
-        // 若没有 this 关键字，则这俩不会变为 Person 的属性，仅仅只是一个赋值操作
+        // 若没有 this 关键字，则这俩不会变为 Person 的属性，仅仅只是一个赋值操作，类似于 Python 中 class 的 self 关键字
         this.name = name;
         this.age = age;
     }
