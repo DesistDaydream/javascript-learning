b9f776f9dc00989e9602a12a3d937984bac56456
DesistDaydream <373406000@qq.com>
2022-04-26 11:47:55 +0800

    ES6 函数

--- 4.function/function.js
+++ 4.function/function.js
@@ -1,10 +1,10 @@
 // 定义函数
-function fn() {
+function fn1() {
     console.log("Function 练习")
 }
 
 // 调用函数
-fn()
+fn1()
 
 // Function 参数
 // JS 里的形参不用指定类型么？~
@@ -32,4 +32,23 @@ function shapeFn(rows, columns) {
     return shapeFn(rows - 1, columns)
 }
 
-shapeFn(3, 3)
+// shapeFn(3, 3)
+
+
+// 箭头函数是函数表达式(匿名函数) 的简写
+// 在 ES2015 中，箭头函数是一种更简洁的写法，使用箭头函数可以让函数体更短。
+// 在 ES2015 中，箭头函数不能用在 new 命令，因为箭头函数没有 this 和 arguments 对象。
+// 在 ES2015 中，箭头函数不能用在 class 内部，因为箭头函数没有 this 和 arguments 对象。
+// 在 ES2015 中，箭头函数不能用在 class 外部，因为箭头函数没有 this 和 arguments 对象。
+var fn1 = () => {
+    console.log("箭头函数")
+}
+
+fn1()
+
+// 函数参数默认值
+function fn2(a, b = 1) {
+    console.log(a, b)
+}
+
+fn2(1)
