83cf0ed581d84ea8ceebc58451dca9c6d27ecfa0
DesistDaydream <373406000@qq.com>
2022-05-05 16:10:15 +0800

    module

--- es_module/index.js
+++ es_module/index.js
@@ -1,6 +1,6 @@
-// 通过包名导入模块
 // 在 Node.js 环境中，可以直接使用模块名称导入模块。
 // 同时，需要在 package.json 添加 "type": "module" 以便让 Node.js 环境知道这是一个 ES6 模块，这样才能执行 import 指令。默认使用的是 CommonJS 模块。
+// 若使用模块名称导入模块，则 Node.js 环境会自动去 node_modules 目录中查找模块。模块名称就是文件夹名称。
 // import {
 //     name as name_one
 // } from 'module_one'
