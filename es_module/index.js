4682803e252ac55f6d7189e71397d1044d8aa67d
DesistDaydream <373406000@qq.com>
2022-05-05 16:01:48 +0800

    完善 ES6 模块说明

--- es_module/index.js
+++ es_module/index.js
@@ -1,10 +1,22 @@
+// 通过包名导入模块
+// 在 Node.js 环境中，可以直接使用模块名称导入模块。
+// 同时，需要在 package.json 添加 "type": "module" 以便让 Node.js 环境知道这是一个 ES6 模块，这样才能执行 import 指令。默认使用的是 CommonJS 模块。
+// import {
+//     name as name_one
+// } from 'module_one'
+
+// 在浏览器环境中，必须使用模块文件的路径导入模块。
+// 否则浏览器将会报错：
+// Uncaught TypeError: Failed to resolve module specifier "module_one". Relative references must start with either "/", "./", or "../".
+// 因为浏览器无法根据模块的名称找到模块文件的路径，所以无法导入模块。
 import {
     name as name_one
-} from './one.js'
+} from './node_modules/module_one/one.js'
 
+// 通过模块文件导入模块
 import {
     name
-} from "./two.js"
+} from "./node_modules/module_two/two.js"
 
 console.log(name_one)
 console.log(name)
