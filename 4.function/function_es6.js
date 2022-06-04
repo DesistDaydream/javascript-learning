55ee25514ef5f643ac35678f3ee61c58b07ce21c
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-06-04 13:16:36 +0800

    Update function_es6.js

--- 4.function/function_es6.js
+++ 4.function/function_es6.js
@@ -1,3 +1,8 @@
+// 函数参数默认值
+function DefaultParameterValue(a, b = 1) {
+    console.log(a, b)
+}
+
 // 箭头函数是函数表达式(匿名函数) 的简写
 // 在 ES6 中，箭头函数是一种更简洁的写法，使用箭头函数可以让函数体更短。
 // 在 ES6 中，箭头函数不能用在 new 命令，因为箭头函数没有 this 和 arguments 对象。
@@ -39,10 +44,5 @@ function ArrowFunction() {
     console.log(f4_1(1, 2))
 }
 
-// 函数参数默认值
-function DefaultParameterValue(a, b = 1) {
-    console.log(a, b)
-}
-
-ArrowFunction()
 // DefaultParameterValue(1)
+ArrowFunction()
