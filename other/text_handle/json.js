58046c19e170ed18a3b64ee990b18def7c1a77c9
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-05-15 17:58:34 +0800

    upgrade

--- other/text_handle/json.js
+++
@@ -1,4 +0,0 @@
-jsonText =
-  '{"name": "DesistDaydream", "body": "Hello World", "time": "2022-04-26 16:10:52"}'
-let res = JSON.parse(jsonText)
-console.log(res)
---
+++ text/json.js
@@ -0,0 +1,13 @@
+// jsonText =
+//   '{"name": "DesistDaydream", "body": "Hello World", "time": "2022-04-26 16:10:52"}'
+// let res = JSON.parse(jsonText)
+// console.log(res)
+
+name = "DesistDaydream"
+age = 22
+jsonText = {
+  name: name,
+  age: age,
+}
+
+console.log(JSON.stringify(jsonText))
